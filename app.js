45d8a364bd084c67d2e9e013716e84c500a9fb6f
patrickrasabonihantana2 <patrickrasabonihantana2@gmail.com>
2023-01-13 00:42:05 +0300

    nodemon + routes

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth/index');
+var apiRouter = require('./routes/api/index');
 
 var app = express();
 
@@ -15,6 +16,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/auth', authRouter);
+app.use('/api', apiRouter);
 
 module.exports = app;
