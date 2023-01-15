c0f9fe9d206b6e5e48f9cabb835d6d9ec37c58cd
patrickrasabonihantana2 <patrickrasabonihantana2@gmail.com>
2023-01-15 13:01:29 +0300

    dao + test

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
+const Env = require('./util/env');
 
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth/index');
@@ -18,5 +20,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/api', apiRouter);
+
+
+const allowedOrigins = (Env.SECURITY_CORS_ALLOW_ORIGIN || '').split(',').map((s) => s.trim());
+const allowedMehods = Env.SECURITY_CORS_ALLOW_METHODS;
+const allowedHeaders = Env.SECURITY_CORS_ALLOW_HEADERS;
+const allowCredentialds = Env.SECURITY_CORS_ALLOW_CREDENTIALS;
+
+// CORS
+app.use(cors({
+  origin: (origin, callback) => {
+    const originAllowed = allowedOrigins.some((o) => {
+      const test = (o) => {
+        try {
+          const r = new RegExp(o, 'i');
+          return r.test(origin);
+        } catch {
+          return false;
+        }
+      };
+      return o === '*' || test(o);
+    });
+
+    if (originAllowed) callback(null, origin);
+    else callback(new Error('not allowed'), []);
+  },
+  methods: allowedMehods,
+  allowedHeaders: allowedHeaders,
+  optionsSuccessStatus: 200,
+  credentials: allowCredentialds,
+}));
 
 module.exports = app;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,24 @@
 var express = require('express');
+const Env = require('../../util/env');
+const MongoConnect = require('../../dao/MongoConnect');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.send('route not implemented');
+router.get('/', async function(req, res, next) {
+  const mongoConnect = new MongoConnect();
+  let mongoClient = undefined;
+  try {
+    mongoClient = await mongoConnect.getConnection();
+    let collection = mongoClient.db('garage_test').collection('users');
+    let result = await collection.find({}).toArray();
+    res.json(result);
+  } catch(err) {
+    throw err;
+  } finally {
+    if(mongoClient) {
+      mongoClient.close();
+    }
+  }
+  console.log('MONGO_URL', Env.MONGO_URL);
 });
 
 module.exports = router;
