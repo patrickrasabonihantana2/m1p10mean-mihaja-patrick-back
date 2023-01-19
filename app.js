247cc96a831151edf1d4bcfea56c6e37df09fa84
patrickrasabonihantana2 <patrickrasabonihantana2@gmail.com>
2023-01-20 02:53:51 +0300

    cors middleware

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require('cors');
-const Env = require('./src/util/env');
+const corsSecurity = require('./src/middlewares/cors');
 
 var indexRouter = require('./src/routes/index');
 var authRouter = require('./src/routes/auth');
@@ -21,34 +20,7 @@ app.use('/', indexRouter);
 app.use('/auth', authRouter);
 app.use('/api', apiRouter);
 
-
-const allowedOrigins = (Env.SECURITY_CORS_ALLOW_ORIGIN || '').split(',').map((s) => s.trim());
-const allowedMehods = Env.SECURITY_CORS_ALLOW_METHODS;
-const allowedHeaders = Env.SECURITY_CORS_ALLOW_HEADERS;
-const allowCredentialds = Env.SECURITY_CORS_ALLOW_CREDENTIALS;
-
 // CORS
-app.use(cors({
-  origin: (origin, callback) => {
-    const originAllowed = allowedOrigins.some((o) => {
-      const test = (o) => {
-        try {
-          const r = new RegExp(o, 'i');
-          return r.test(origin);
-        } catch {
-          return false;
-        }
-      };
-      return o === '*' || test(o);
-    });
-
-    if (originAllowed) callback(null, origin);
-    else callback(new Error('not allowed'), []);
-  },
-  methods: allowedMehods,
-  allowedHeaders: allowedHeaders,
-  optionsSuccessStatus: 200,
-  credentials: allowCredentialds,
-}));
+app.use(corsSecurity);
 
 module.exports = app;
